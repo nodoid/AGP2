baa8d600ace2ce5193738ec129f308149d490f1c
nodoid <paul@snuberry-software.co.uk>
2015-02-15 21:48:20 +0000

    Initial import

---
+++ AGP2/AGP2/GameView.cs
@@ -0,0 +1,47 @@
+﻿using Android.Views;
+using Android.Content;
+using Android.Graphics;
+
+namespace AGP2
+{
+    public class GameView : SurfaceView, ISurfaceHolderCallback
+    {
+
+        Bitmap bmp;
+        ISurfaceHolder holder;
+
+        public GameView(Context context)
+            : base(context)
+        {
+            holder = Holder;
+
+            holder.AddCallback(this);
+
+            bmp = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Icon);
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            canvas.DrawColor(Color.Argb(255, 68, 119, 102));
+            canvas.DrawBitmap(bmp, 50, 100, null);
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, Format format, int width, int height)
+        {
+
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            var c = holder.LockCanvas(null);
+            OnDraw(c);
+            holder.UnlockCanvasAndPost(c);
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+
+        }
+    }
+}
+
---
+++ AGP2/AGP2/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AGP2
+{
+    [Activity(Label = "AGP2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            RequestWindowFeature(WindowFeatures.NoTitle);
+            SetContentView(new GameView(this));
+        }
+    }
+}
+
+
